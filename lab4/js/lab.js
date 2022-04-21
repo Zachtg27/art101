4d106db93a44329f010f4e6ee7530b36637daf53
Zachtg27 <103210735+Zachtg27@users.noreply.github.com>
2022-04-20 23:18:38 -0700

    finished lab 5

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+/**
+ * Pseudocode lab.
+ *
+ * Practice writing pseudocode for labs.
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author ZacharyThompson.
+ * @since  4.18.22
+ */
+
+// Take in word from user
+// Take in encode key from user
+      // Convert encoded word to its ascii values
+      // Convert encode key into ascii values (if need)
+          // Add ascii values of encode key to word
+          // Convert encoded word from ascii to letters
+// Return the encoded word to user
---
+++ lab5/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Data Types and Variables Lab.
+ *
+ * Learn about javascript and javascript variables.
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author ZacharyThompson.
+ * @since  4.20.22
+ */
+
+ // define Variables
+ make = "Honda";
+ model = "Accord";
+ color = "Space Grey";
+ year = "2008";
+
+ // calc
+ age = 2022 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "years<br>");
