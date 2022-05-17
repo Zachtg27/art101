f6ba214d8d0c9feb5f1b562ff3fa1b3aadfd4099
Zachtg27 <103210735+Zachtg27@users.noreply.github.com>
2022-05-17 01:42:55 -0700

    finished lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,36 @@
+/**
+ * Conditionals
+ *
+ * Learn more about Conditionals.
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author ZacharyThompson.
+ * @since  5.16.22
+ */
+
+// return Harry Potter Houses depending on length and mod 4
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+})
